4ee4b4777551766c4aeeb02f0d843ab8869146e0
jinkim31 <jinkim5771@gmail.com>
2023-08-10 00:11:22 +0900

    update

--- main.cpp
+++ main.cpp
@@ -7,7 +7,6 @@ int main(int , char *[])
 {
     EThread mainThread;
     EThread::provideMainThread(mainThread);
-    mainThread.setLoopPeriod(std::chrono::milliseconds(0));
     Application app;    // hangs here until GUI closes
     app.removeFromThread();
     return 0;
